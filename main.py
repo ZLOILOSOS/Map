adc26a1cc7b61dab65db059c44febaf4334785ad
s.martha <martha.stepashkina@yandex.ru>
2022-02-10 19:58:04 +0300

    6 задача 1 и задача 2

--- main.py
+++ main.py
@@ -1,74 +1,88 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap
-from PIL import Image
+import os
+import sys
+import math
+import requests
 import geocoder
 import distance
 import business
-import sys
+from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap
+from PIL import Image
 from io import BytesIO
 
-import requests
+SCREEN_SIZE = [600, 450]
+a = 1
+LAT_STEP = 0.002
+LON_STEP = 0.002
+coord_to_geo = 0.0000428
 
-api_server = "http://static-maps.yandex.ru/1.x/"
-
-lon = "37.530887"
-lat = "55.703118"
-delta = "0.002"
-
-params = {
-    "ll": ",".join([lon, lat]),
-    "spn": ",".join([delta, delta]),
-    "l": "map"
-}
-response = requests.get(api_server, params=params)
-
-toponym_to_find = " ".join(sys.argv[1:])
-
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-
-geocoder_params = {
-    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-    "geocode": toponym_to_find,
-    "format": "json"}
-response = requests.get(geocoder_api_server, params=geocoder_params)
+my_object = (37.530887, 55.703118)
 
 
-if not response:
-    pass
-
-json_response = response.json()
-toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-toponym_coodrinates = toponym["Point"]["pos"]
-toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-
-delta = "0.005"
-
-map_params = {
-    "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join([delta, delta]),
-    "l": "map"
-}
-
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(map_api_server, params=map_params)
-
-Image.open(BytesIO(
-    response.content)).show()
-
-
-class Map(QWidget):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('map.ui', self)
-        self.textEdit.setReadOnly(True)
-        [i.clicked.connect(self.run) for i in self.buttonGroup.buttons()]
+        self.map_file = "map.png"
+        self.lon = my_object[0]
+        self.lat = my_object[1]
+        self.z = 14
+        self.lat_step = LAT_STEP
+        self.lon_step = LON_STEP
+        self.initUI()
+        self.view_image()
 
-        self.pixmap = QPixmap(response)
+    def getImage(self):
+        params = {
+            'll': f'{self.lon},{self.lat}',
+            'z': self.z,
+            'l': 'map'
+        }
+        map_request = "http://static-maps.yandex.ru/1.x/"
+        response = requests.get(map_request, params=params)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def view_image(self):
+        self.getImage()
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Большая задача по Maps')
+
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(SCREEN_SIZE[0], SCREEN_SIZE[1])
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp and self.z < 20:
+            self.z += 1
+            self.lat_step /= 2
+            self.lon_step /= 2
+            self.view_image()
+        elif event.key() == Qt.Key_PageDown and self.z > 0:
+            self.z -= 1
+            self.lat_step *= 2
+            self.lon_step *= 2
+            self.view_image()
+
+
+    def closeEvent(self, event):
+        os.remove(self.map_file)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Map()
-    ex.show()
-    sys.exit(app.exec_())
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
