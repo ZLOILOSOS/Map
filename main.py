afc2f9da32c8e4c870642a16a35e8e44e53e9b6f
ZLOILOSOS <mirzaevbogdan01@gmail.com>
2022-02-10 19:20:22 +0300

    2

--- main.py
+++ main.py
@@ -13,8 +13,57 @@ import requests
 from PIL import Image
 
 
+class Map(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('map.ui', self)
+        self.textEdit.setReadOnly(True)
+        [i.clicked.connect(self.run) for i in self.buttonGroup.buttons()]
+
+
+import sys
+from io import BytesIO
+
+import requests
+from PIL import Image
+
+toponym_to_find = " ".join(sys.argv[1:])
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": toponym_to_find,
+    "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+
+if not response:
+    pass
+
+json_response = response.json()
+toponym = json_response["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]
+toponym_coodrinates = toponym["Point"]["pos"]
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+delta = "0.005"
+
+map_params = {
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "spn": ",".join([delta, delta]),
+    "l": "map"
+}
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+response = requests.get(map_api_server, params=map_params)
+
+Image.open(BytesIO(
+    response.content)).show()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Menu()
+    ex = Map()
     ex.show()
-
+    sys.exit(app.exec_())
